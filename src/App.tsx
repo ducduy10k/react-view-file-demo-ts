4dca0f24f9ffffd8721d8ef927a6af90da920d3d
rikkei-duynd <duynd@rikkeisoft.com>
2022-08-08 10:00:56 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 
 function App() {
+  const docs = [
+    { uri: require('./s01-redux-overview.pdf') }, // Local File
+    { uri: require('./dailymeeting.txt') }, // Local File
+    { uri: require('./tesst.docx') }, // Local File
+    { uri: require('./adasdas.pptx') }, // Local File
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DocViewer
+      documents={docs}
+      pluginRenderers={DocViewerRenderers}
+    ></DocViewer>
   );
 }
 
